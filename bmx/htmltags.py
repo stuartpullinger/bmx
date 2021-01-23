ad3555913902c12064dd41e7220fbb0e3ce7509e
Stuart Pullinger <stuartpullinger@gmail.com>
2021-01-23 20:23:16 +0000

    Add DOCTYPE to htmltags module

--- bmx/htmltags.py
+++ bmx/htmltags.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from .core import SelfClosingTag, SelfClosingTagStyle, Tag
+from .core import SelfClosingTag, SelfClosingTagStyle, Tag, DOCTYPE
 
 # <!--...-->
 pass
